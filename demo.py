e868bfad128baaaf03e6f2a324c5f325c91ca738
pratyush <pratyushpathak0208@gmail.com>
2023-09-12 02:01:49 -0700

    add file

--- demo.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title("Hello :wave:")
-st.write("---")
-st.write("_streamlit_ lib is very fast to **build** data apps")
-st.write("[contact me](https://www.linkedin.com/in/pratyush-pathak/)")
-st.divider()
--- streamlit.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title("hello :wave:")
-st.divider()
-st.write("hii how are you all")
-st.divider()
-st.markdown("[linkedin](https://www.linkedin.com/in/pratyush-pathak/)")
